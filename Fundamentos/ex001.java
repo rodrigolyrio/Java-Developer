139abb00db394b8a9ed228c77a26deb5013c8c4b
Rodrigo Lyrio <rodrigolyrio2009@gmail.com>
2025-09-25 01:59:52 -0300

    Update ex001.java

--- Fundamentos/ex001.java
+++ Fundamentos/ex001.java
@@ -1,4 +1,5 @@
 public class ex001 {
+    // define uma função que cria uma etiqueta de produto. A função gerarEtiqueta recebe o nome do produto e seu ano de lançamento como entrada e retorna uma string formatada
     public static void main(String[] args) {
         String etiqueta = gerarEtiqueta("Notebook Gamer", 2025);
         System.out.println(etiqueta);
