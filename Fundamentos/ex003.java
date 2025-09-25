5048c7d9efab2392c3f9b07790c2bee63597d878
Rodrigo Lyrio <rodrigolyrio2009@gmail.com>
2025-09-25 02:01:34 -0300

    Update ex003.java

--- Fundamentos/ex003.java
+++ Fundamentos/ex003.java
@@ -1,4 +1,5 @@
 public class ex003 {
+    // Este programa simula a análise de desempenho de um vendedor. Ele utiliza operadores lógicos (&& e ||) para verificar se o vendedor atingiu a meta e/ou se a nota de satisfação do cliente é alta, imprimindo mensagens diferentes para cada cenário.
     public static void main(String[] args) {
         boolean bateuMeta = true;
         double satisfacaoCliente = 7.5;
@@ -14,4 +15,5 @@ public class ex003 {
         System.out.println("Análise de performance finalizada.");
     }
 }
+
 
