b5e59cd0ab3d704c2e7d8fb674ac098dc1aaaf87
Rodrigo Lyrio <rodrigolyrio2009@gmail.com>
2025-09-25 00:04:27 -0300

    Create main.java

---
+++ Fundamentos/ex005/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        ValidadorDeChave validador = new ValidadorDeChave();
+        boolean ehValida = validador.validarChave(112345);
+    }
+}
