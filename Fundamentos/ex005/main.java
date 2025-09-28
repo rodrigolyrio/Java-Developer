081f886ee38aa21557574a0deeb07585ebaa9e7c
Rodrigo Lyrio <rodrigolyrio2009@gmail.com>
2025-09-28 16:46:52 -0300

    Delete Fundamentos directory

--- Fundamentos/ex005/main.java
+++
@@ -1,6 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        ValidadorDeChave validador = new ValidadorDeChave();
-        boolean ehValida = validador.validarChave(112345);
-    }
-}
