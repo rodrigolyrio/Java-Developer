849624e6fd343c84381c3db1addd4c85dd82bd4a
Rodrigo Lyrio <rodrigolyrio2009@gmail.com>
2025-10-02 10:47:28 -0300

    Add files via upload

---
+++ 02. Escopo e Estruturas de Controle/ex004.java
@@ -0,0 +1,8 @@
+public class ex004 {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Foguete lançado!");
+    }
+}
