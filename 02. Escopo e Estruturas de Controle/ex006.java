286c77774e1d1b0e81cdaff544da14902795dd81
Rodrigo Lyrio <rodrigolyrio2009@gmail.com>
2025-11-07 15:08:33 -0300

    Delete 02. Escopo e Estruturas de Controle directory

--- 02. Escopo e Estruturas de Controle/ex006.java
+++
@@ -1,9 +0,0 @@
-public class ex006 {
-    public static void main(String[] args) {
-        String[] nomes = {"ANA", "PEDRO", "MARIA", "CARLOS"};
-
-        for (int i = (nomes.length - 1); i >= 0;  i--) {
-            System.out.println(nomes[i]);
-        }
-    }
-}
